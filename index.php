5ae5ad50f978705fef875b7911f6816dd2121200
Irwan Desktop <xirwanx@gmail.com>
2022-10-14 14:58:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package WordPress
- * @since Holi Moli 1.0
+ * @since Website 1.0
 */
 ?><!doctype html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
@@ -9,6 +9,16 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<meta name="format-detection" content="telephone=no">
+        <title><?php
+		global $page, $paged;
+		wp_title( '|', true, 'right' );
+		bloginfo( 'name' );
+		$site_description = get_bloginfo( 'description', 'display' );
+		if ( $site_description && ( is_home() || is_front_page() ) )
+		    echo " | $site_description";
+		?></title>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 	<?php wp_head(); ?>
 </head>
