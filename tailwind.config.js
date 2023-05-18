3d5d3d5d097638e84a47961bbc6e0365f9c740e5
Irwan Desktop <xirwanx@gmail.com>
2023-05-18 18:18:25 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 const colors = require('tailwindcss/colors');
 module.exports = {
 	purge: {
-		mode: 'layers',
 		content: ['./*.php', './inc/**/*.php', './template-parts/**/*.php', './assets/**/*.js'],
 	},
 	darkMode: false, // or 'media' or 'class'
